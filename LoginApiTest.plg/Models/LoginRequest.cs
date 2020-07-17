38976528570f45728fddbff3673ad1a4133884a3
Tasibaev Davron <tdav@mail.ru>
2020-07-17 12:46:22 +0500

    renameq

---
+++ LoginApiTest.plg/Models/LoginRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace T1.plg.Models
+{
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
---
+++ T1.plg/Models/LoginResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace T1.plg.Models
+{
+    public class LoginResponse
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Phone { get; set; }
+        public int RoleId { get; set; }
+        public decimal Cash { get; set; }
+        public string Token { get; set; }
+
+        public int Status { get; set; }
+        public string StatusMessage { get; set; }
+    }
+
+}
